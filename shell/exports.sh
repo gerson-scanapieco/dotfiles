7eaf93ceb2ef504e01e88b8b311ffa2c95414f17
Gerson Scanapieco <gerson.scanapieco@gmail.com>
2018-04-02 18:04:23 -0300

    Add variable PG_HOME in exports.sh

--- shell/exports.sh
+++ shell/exports.sh
@@ -8,6 +8,10 @@ export HISTFILE="$HOME/.zsh_history"
 export HISTCONTROL="ignoreboth:erasedups" # Erase duplicates in history
 export HISTSIZE=10000 # Store 10k history entries
 
+export PG_HOME="/usr/local/opt/postgresql@9.6"
+
+export PATH="$PG_HOME/bin:$PATH"
+
 if [ $ITERM_SESSION_ID ]; then
   export PROMPT_COMMAND='echo -ne "\033];${PWD##*/}\007"; ':"$PROMPT_COMMAND";
 fi
