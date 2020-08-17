61c6f123c62e02c6d1ca226400640de3d1e7b699
Gerson Scanapieco <gerson.scanapieco@gmail.com>
2020-08-17 15:05:01 -0300

    Uses vscode as default editor for bundler

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,16 +1,12 @@
 # Sublime as default editor.
-export BUNDLER_EDITOR="subl"
-export GEM_EDITOR="subl"
-export EDITOR="subl -w"
+export BUNDLER_EDITOR="code"
+export GEM_EDITOR="code"
+export EDITOR="code -w"
 export VISUAL="$EDITOR"
 
 export HISTFILE="$HOME/.zsh_history"
 export HISTCONTROL="ignoreboth:erasedups" # Erase duplicates in history
 export HISTSIZE=10000 # Store 10k history entries
-
-export PG_HOME="/usr/local/opt/postgresql@9.6"
-
-export PATH="$PG_HOME/bin:$PATH"
 
 if [ $ITERM_SESSION_ID ]; then
   export PROMPT_COMMAND='echo -ne "\033];${PWD##*/}\007"; ':"$PROMPT_COMMAND";
